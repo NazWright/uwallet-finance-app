68a0ac8686066875e923a8ead93349b6b75965d5
Nazere Wright <nazerewright@Nazeres-MacBook-Pro.local>
2023-09-30 10:38:55 -0400

    initializing amplify and backend and adding amplify authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myexamplerepo12907d6f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
