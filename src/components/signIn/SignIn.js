0449398a7bf178206f0e83b270a32ea0ce1da03a
Nazere Wright <nazwrightthedeveloper@gmail.com>
2023-11-14 21:06:25 -0500

    Removing signin verification code.
    Will not require verification code to sign in currently.

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,25 +1,7 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import Verification from "../shared/verification/Verification";
-import { Auth } from "aws-amplify";
+import React from "react";
 import SignInContent from "./SignInContent";
 
 export default function SignIn() {
-  //   const [needsVerification, setNeedsVerification] = useState(false);
-  //   const dispatch = useDispatch();
-
-  //   const submitVerificationCode = async (data) => {
-  //     // confirm sign in
-  //     // set authenticated to true
-  //     if (!data.verificationCode) return;
-  //     try {
-  //       const authenticatedUserMeta = await Auth.currentAuthenticatedUser();
-  //       console.log(authenticatedUserMeta);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-
   return (
     <div>
       <SignInContent />
