3442892c3899738dd99cbc78752e340ee93eef0e
Nazere Wright <nazwrightthedeveloper@gmail.com>
2024-01-28 14:01:23 -0500

    New frontend form validation to AddNewCardStepForm. Now logs proper form values when user submits the form.

--- src/components/onboarding/steps/AddNewCardStepForm.js
+++ src/components/onboarding/steps/AddNewCardStepForm.js
@@ -11,8 +11,8 @@ export default function AddNewCardStepForm({ handleCompletedStep }) {
     formState: { errors },
   } = useForm();
 
-  function submitNewCardInformation(event) {
-    console.log("yo");
+  function submitNewCardInformation(data) {
+    console.log(data);
     // store info in redux on every change
     handleCompletedStep();
   }
@@ -25,31 +25,56 @@ export default function AddNewCardStepForm({ handleCompletedStep }) {
     <>
       <form onSubmit={handleSubmit(submitNewCardInformation)}>
         {/* card number field */}
-        <input
-          className="element-wrapper"
-          name="cardNumber"
-          type="number"
-          max={16}
-          min={16}
-        />
+        <div className="element-wrapper">
+          <input
+            name="cardNumber"
+            type="number"
+            className="uwallet-input h-100 w-100"
+            maxLength={16}
+            minLength={16}
+            {...register("cardNumber", { required: true })}
+          />
+          {errors["cardNumber"] && <span className="error-icon">x</span>}
+        </div>
         {/* Card number field */}
         <div className="text-wrapper-20">Card Number</div>
 
         <div className="group-13">
-          <input className="overlap-9" name="exp" />
-          <input
-            className="overlap-10"
-            name="cvv"
-            type="number"
-            max={3}
-            min={3}
-          />
+          <div className="overlap-9 d-flex overflow-hidden">
+            <input
+              className="uwallet-input overflow-hidden"
+              name="exp"
+              {...register("exp", { required: true })}
+            />
+            {errors["exp"] && <span className="error-icon">x</span>}
+          </div>
+
+          <div className="overlap-10 d-flex">
+            <input
+              name="cvv"
+              className="uwallet-input h-100 overflow-hidden d-flex align-items-center"
+              type="number"
+              maxLength={3}
+              minLength={3}
+              {...register("cvv", { required: true })}
+            />
+            {errors["cvv"] && <span className="error-icon">x</span>}
+          </div>
           <div className="text-wrapper-23">CVV</div>
           <div className="text-wrapper-24">EXP</div>
         </div>
         <div className="overlap-11">
           <div className="group-14">
-            <input className="rectangle-8" name="nameOnCard" />
+            <div className="rectangle-8 d-flex">
+              <input
+                className="uwallet-input h-100"
+                name="cardHolderName"
+                {...register("cardHolderName", { required: true })}
+              />
+              {errors["cardHolderName"] && (
+                <span className="error-icon">x</span>
+              )}
+            </div>
             <div className="text-wrapper-24">Name on Card </div>
           </div>
         </div>
